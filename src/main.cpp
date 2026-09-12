6686c0d913dec9cbe6a6fc2243c33fc1e126aaaa
Barkosss <andrey.bar66@gmail.com>
2026-09-12 08:36:28 +0500

    feat: register HTTP routes

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,57 @@
-//
-// Created by Andrey Baryshev on 03.05.2026.
-//
+#include <iostream>
+#include <functional>
 
-#include<iostream>
-#include <exception>
+#include "utils/config.h"
 
-#include "server/Router.h"
-#include "server/Server.h"
-#include "./handlers/manifest_handler.h"
-#include "./handlers/health_handler.h"
-#include "./handlers/file_handler.h"
-#include "./handlers/sync_handler.h"
+#include "server/router.h"
+#include "server/server.h"
 
-int main() {
+#include "handlers/healthzHandler.h"
+#include "handlers/manifestHandler.h"
+#include "handlers/downloadHandler.h"
+#include "handlers/checkHandler.h"
+#include "handlers/reloadHandler.h"
+
+using boost::asio::ip::tcp;
+
+int main(int argc, char* argv[]) {
+    uint16_t port = 0;
+
+    if (argc > 1) {
+        try {
+            port = static_cast<uint16_t>(std::stoi(argv[1]));
+        } catch (const std::exception& ex) {
+            std::cerr << "Invalid port number: " << argv[1] << std::endl;
+            return 1;
+        }
+    }
+
+    if (!port && Config::getInstance().has("server.port")) {
+        port = Config::getInstance().get<uint16_t>("server.port");
+    }
+
+    if (!port) {
+        std::cerr << "Port number not specified. Please provide a port number as a command-line argument or in the config file." << std::endl;
+        return 1;
+    }
 
     try {
-        Router router;
+        boost::asio::io_context io_context{};
 
-        router.addRoute(http::verb::get, "/manifest", handleManifest);
-        router.addRoute(http::verb::get, "/file", handleFile);
-        router.addRoute(http::verb::get, "/health", handleHealth);
-        router.addRoute(http::verb::get, "/sync", handleSync);
+        Router::getInstance().addRoute(http::verb::get, "/healthz", healthzHandler);
+        Router::getInstance().addRoute(http::verb::get, "/manifest", manifestHandler);
+        Router::getInstance().addRoute(http::verb::post, "/download", downloadHandler); // TODO
+        Router::getInstance().addRoute(http::verb::post, "/check", checkHandler); // TODO
+        Router::getInstance().addRoute(http::verb::get, "/reload", reloadHandler);
 
-        Server server(router, 8080);
-        server.run();
+        Server server(io_context, port);
 
-    } catch (std::exception &ex) {
-        std::cerr << ex.what() << std::endl;
+        std::cout << "Server is running on port (:" << port << ")" << std::endl;
+        io_context.run();
+
+    } catch (const std::exception& ex) {
+        std::cerr << "Error: " << ex.what() << std::endl;
+        return 1;
     }
 
     return 0;
